1e93b64e9730dfb2a030565481b3563b2b5ec486
Schattenbrot <aaron.machill@gmail.com>
2025-09-28 18:01:47 +0200

    fix android to have an Android Home

--- android/flake.nix
+++ android/flake.nix
@@ -11,19 +11,33 @@
 			forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
 				pkgs = import nixpkgs { 
           inherit system; 
-          config.allowUnfree = true;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
         };
 			});
 		in
 		{
-			devShells = forEachSupportedSystem ({ pkgs }: {
+			devShells = forEachSupportedSystem ({ pkgs }: 
+        let
+          androidSdk = (pkgs.androidenv.composeAndroidPackages {
+            platformVersions = [ "35" ];
+            abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          }).androidsdk;
+        in {
 				default = pkgs.mkShell {
 					packages = with pkgs; [ 
             android-studio
             android-tools
 						gradle
 						jdk17
+            androidSdk
           ];
+          shellHook = ''
+            export ANDROID_HOME=${androidSdk}/libexec/android-sdk
+            export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
+          '';
 				};
 			});
 		};
