1d47e5b57f7b4d1dde500f5bb711590d37090f03
Schattenbrot <aaron.machill@gmail.com>
2025-09-28 15:32:18 +0200

    add go version 1.25

---
+++ go/1.25/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake with Go and it's development tools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 25;
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+        forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+        });
+    in
+      {
+        overlays.default = final: prev: {
+          go = final."go_1_${toString goVersion}";
+        };
+
+        devShells = forEachSupportedSystem ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gotools
+            ];
+          };
+        });
+      };
+}
